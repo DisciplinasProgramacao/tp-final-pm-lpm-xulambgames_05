966f0bc3c982ed29a0df0720355a36ec9612f5ae
Gustavo Nogueira <69543216+gustavocn121@users.noreply.github.com>
2022-06-06 19:43:13 -0300

    Add files via upload

---
+++ src/TipoJogo.java
@@ -0,0 +1,23 @@
+public enum TipoJogo {
+    LANCAMENTO(0, "Lancamento"),
+    PREMIUM(1, "Premium"),
+    REGULAR(2, "Regular"),
+    PROMOCAO(3, "Promocao");
+    int tipo;
+    String descricao;
+    
+    TipoJogo(int tipo, String descricao){
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+
+    public int getTipo(){
+        return this.tipo;
+    }
+
+    @Override
+    public String toString(){
+        return this.name().toLowerCase();
+    }
+
+}
