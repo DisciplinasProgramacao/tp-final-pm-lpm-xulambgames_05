6f2b8b8ab8fa9c0a4ed3268e467f3f5e03f817e7
Gustavo Nogueira <69543216+gustavocn121@users.noreply.github.com>
2022-06-06 18:43:29 -0300

    Add files via upload

--- src/Venda.java
+++ src/Venda.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 
 public class Venda implements IDescontavel{
     private ArrayList<Jogo> jogosList;
-    private Date dataVenda;
+    private Data dataVenda;
     private double descontoAplicado;
 
+    
+
+    public Venda(ArrayList<Jogo> jogosList, Data dataVenda, double descontoAplicado) {
+        this.jogosList = jogosList;
+        this.dataVenda = dataVenda;
+        this.descontoAplicado = descontoAplicado;
+    }
 
 
     public double calcularValor(){
@@ -17,11 +24,63 @@ public class Venda implements IDescontavel{
         return valor;
     }
 
+
     @Override
     public double calcularDesconto() {
-        return 0;
+        double desconto = 0;
+        HashMap<Integer,Integer> mapaJogos = new HashMap<Integer, Integer>();
+
+        mapaJogos.put(0, 0);
+        mapaJogos.put(1, 0);
+        mapaJogos.put(2, 0);
+        mapaJogos.put(3, 0);
+
+        for (Jogo jogo : jogosList) {
+            if (jogo.getTipoJogo().getTipo() == 0) {
+                mapaJogos.put(0,  mapaJogos.get(0) + 1);
+            }else{
+                if (jogo.getTipoJogo().getTipo() == 1) {
+                    mapaJogos.put(1, mapaJogos.get(1) + 1);
+                }else{
+                    if (jogo.getTipoJogo().getTipo() == 2) {
+                        mapaJogos.put(2, mapaJogos.get(2) + 1);
+                    }else{
+                        if (jogo.getTipoJogo().getTipo() == 3) {
+                            mapaJogos.put(3, mapaJogos.get(3) + 1);
+                        }
+                    }
+                }
+            }
+            
+            
+        }
+        
+        if (mapaJogos.get(1) >= 2 || mapaJogos.get(2) >= 4) {
+            desconto = 0.1;
+        }
+        
+        /*    
+            LANCAMENTO(0),
+            PREMIUM(1),
+            REGULAR(2),
+            PROMOCAO(3);
+         
+            Contém três regulares e um acima 
+        */
+        
+
+        if (mapaJogos.get(0)>=2 || 
+            mapaJogos.get(1)>=3 || 
+            mapaJogos.get(2)>=5 ||
+            (mapaJogos.get(1)>=2 && jogosList.size() >=3) ||
+            (mapaJogos.get(2)>=3 && jogosList.size() >=4)
+            ) {
+            desconto = 0.2;
+        }
+
+        
+        return desconto;
     }
-
-
-
+    
 }
+
