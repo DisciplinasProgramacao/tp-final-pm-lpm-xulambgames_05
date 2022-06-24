b70456ce2872545e913ca0c54c989c16137a29e1
Gustavo Nogueira <gustavo.cn121@gmail.com>
2022-06-24 01:17:35 -0300

    Add filtro no historico do cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,9 @@
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Cliente implements Serializable{
 
@@ -50,22 +54,102 @@ public class Cliente implements Serializable{
         this.vendasList.add(venda);
     }
 
-    public String historicoCompras(){
+    public String historicoCompras(Scanner teclado, boolean filtrarData, boolean filtrarTipoJogo ){
+        LocalDate dataIni = LocalDate.MIN;
+        LocalDate dataFim = LocalDate.MAX;
+        ArrayList<Venda> vendas = this.vendasList;
+
+        while (filtrarData) {
+            try {
+                System.out.println("\nData inicio historico (formato: dd/MM/yyyy, exemplo: 23/06/2022): ");
+                String inicio = teclado.nextLine();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+                dataIni = LocalDate.parse(inicio, formatter);
+                System.out.println("\nData fim historico (formato: dd/MM/yyyy, exemplo: 23/06/2022): ");
+                String fim = teclado.nextLine();
+                dataFim = LocalDate.parse(fim, formatter);
+                filtrarData = false;
+            } catch (Exception e) {
+                System.out.println("Nao foi possivel filtrar a data");
+            }
+            
+        }
+         
+
+
+
+        
+
+        if(filtrarTipoJogo) {
+            System.out.println("\nDigite uma das opções para o tipo do jogo: ");
+            int tipo = 0;
+            TipoJogo tipoJogoFiltro = null;
+            do {
+                System.out.println("\n1 - LANCAMENTO");
+                System.out.println("\n2 - PREMIUM");
+                System.out.println("\n3 - REGULAR");
+                System.out.println("\n4 - PROMOCAO\n");
+                try{
+                    tipo = teclado.nextInt();
+                    teclado.nextLine();
+                    switch (tipo) {
+                        case 1:
+                        tipoJogoFiltro = TipoJogo.LANCAMENTO;
+                            break;
+                        case 2:
+                        tipoJogoFiltro = TipoJogo.PREMIUM;
+                            break;
+                        case 3:
+                        tipoJogoFiltro = TipoJogo.REGULAR;
+                            break;
+                        case 4:
+                        tipoJogoFiltro = TipoJogo.PROMOCAO;
+                            break;
+                        default:
+                            tipo = 0;
+                            System.out.println("\nSelecione uma das opcoes apresentadas! ");
+                            break;
+                    }
+                }catch(InputMismatchException ex){
+                    teclado.nextLine();
+                    System.out.println("Entrada invalida");
+                }
+            } while (tipo == 0);
+            ArrayList<Venda> vendasFiltrado = new ArrayList<Venda>();
+            for(Venda v: vendas ){
+                boolean contemTipoJogo = false;
+                for (Jogo j : v.getJogosList()) {
+                    if(j.getTipoJogo().name().equals(tipoJogoFiltro.name())){
+                        contemTipoJogo = true;
+                        break;
+                    }
+                }
+                if (contemTipoJogo) {
+                    vendasFiltrado.add(v);
+                }
+            }
+            vendas = vendasFiltrado;
+        }
+
         String s = "-----\tHistorico de vendas do cliente de codigo = "+ this.getCodigo() +"\t-----\n\n" ;
         if (this.vendasList.size() == 0) {
             s += "Nenhuma venda foi realizada por esse cliente\n";
         }
 
-        for (Venda v : this.vendasList) {
-            s+= v.toString();
-            s+= "\n";
+        for (Venda v : vendas) {
+            if (!(v.getDataVenda().isAfter(dataFim)) && !(v.getDataVenda().isBefore(dataIni))) {
+                
+                s+= v.toString();
+                s+= "\n";
+            }
         }
         
         s += "\n----- Fim Historico -----";
         
         return s;
     }
-
+    
+    
     @Override
     public String toString(){
         return this.codigo +" - " + this.nome + " - " + this.tipoCliente.name().toUpperCase(); 
